fb3690ecb9ec2710943d2c43eae0d5d8f500c44c
dlgeraghty <dvd.loge@gmail.com>
2022-12-25 11:00:47 +0100

    create post entity

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -6,3 +6,15 @@ values(10002, current_date(), 'pepe');
 
 insert into user_details(id,birth_date,name)
 values(10003, current_date(), 'alfonso');
+
+insert into post(id,description,user_id)
+values(20001, 'I want to learn aws', 10001);
+
+insert into post(id,description,user_id)
+values(20002, 'I want to learn devops', 10001);
+
+insert into post(id,description,user_id)
+values(20003, 'I want to learn AWS Certified', 10002);
+
+insert into post(id,description,user_id)
+values(20004, 'I want to learn Multicloud', 10002);
