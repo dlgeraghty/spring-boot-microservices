330db4a0447d8d672df442ef8835c8e2492660a0
dlgeraghty <dvd.loge@gmail.com>
2022-12-30 04:41:08 -0500

    config-server setup and configuration

--- limits-service/src/main/java/com/learningspring/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/learningspring/microservices/limitsservice/LimitsServiceApplication.java
@@ -3,6 +3,7 @@ package com.learningspring.microservices.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LimitsServiceApplication {
 
